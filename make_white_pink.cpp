e0e956a030e521ed6cade27e6b6e666d18a23c14
maxCut <maxCut@github.com>
2019-04-10 15:29:08 -0400

    added color slider

--- make_white_pink.cpp
+++ make_white_pink.cpp
@@ -1,6 +1,14 @@
 #include <opencv2/opencv.hpp>
 #define FPS 60
 
+int color_slider = 80;
+const int slider_max = 255;
+
+void on_trackbar( int, void* )
+{
+}
+
+
 int main()
 {
     cv::VideoCapture cap(0);
@@ -8,13 +16,20 @@ int main()
     cv::Mat grayscale;
     cv::Mat mask;
 
+    char TrackbarName[50] ;
+
+    sprintf( TrackbarName, "Color Control");
+
+    cv::namedWindow("Hello World", 1);
+    cv::createTrackbar( TrackbarName, "Hello World", &color_slider, slider_max, on_trackbar );
+
     while(true)
     {
         cap>>image;
         cv::cvtColor(image,grayscale,CV_BGR2GRAY);
 
         cv::inRange(image,cv::Scalar(130,130,130),cv::Scalar(255,255,255),mask);
-        image.setTo(cv::Scalar(80,80,255),mask);
+        image.setTo(cv::Scalar(color_slider,color_slider,255),mask);
         
         cv::imshow("Hello World",image);
 
